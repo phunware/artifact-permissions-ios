a8dc3a7b8433d13bca4d0f5a8c7e8b57c5a2abcc
jenkins <hostmaster@phunware.com>
2025-05-23 06:32:07 -0700

    Release version 1.5.0. See CHANGELOG for details.

---
+++ Frameworks/PhunwareSpeechRecognitionPermission.xcframework/ios-arm64/PhunwareSpeechRecognitionPermission.framework/Headers/PhunwareSpeechRecognitionPermission.h
@@ -0,0 +1,19 @@
+//
+//  PhunwareSpeechRecognitionPermission.h
+//  PhunwareSpeechRecognitionPermission
+//
+//  Created by Kent Tu on 5/6/25.
+//  Copyright © 2025 Phunware, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PhunwareSpeechRecognitionPermission.
+FOUNDATION_EXPORT double PhunwareSpeechRecognitionPermissionVersionNumber;
+
+//! Project version string for PhunwareSpeechRecognitionPermission.
+FOUNDATION_EXPORT const unsigned char PhunwareSpeechRecognitionPermissionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PhunwareSpeechRecognitionPermission/PublicHeader.h>
+
+
