44639e35625c0362b18519a4e9d672134dc2da57
jenkins <hostmaster@phunware.com>
2025-05-23 18:45:03 -0700

    Release version 1.5.0. See CHANGELOG for details.

---
+++ Frameworks/PhunwareBluetoothPermission.xcframework/ios-arm64/PhunwareBluetoothPermission.framework/Headers/PhunwareBluetoothPermission.h
@@ -0,0 +1,19 @@
+//
+//  PhunwareBluetoothPermission.h
+//  PhunwareBluetoothPermission
+//
+//  Created by Kent Tu on 3/17/25.
+//  Copyright © 2025 Phunware, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PhunwareBluetoothPermission.
+FOUNDATION_EXPORT double PhunwareBluetoothPermissionVersionNumber;
+
+//! Project version string for PhunwareBluetoothPermission.
+FOUNDATION_EXPORT const unsigned char PhunwareBluetoothPermissionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PhunwareBluetoothPermission/PublicHeader.h>
+
+
